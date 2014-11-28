faa8d8fd610808fdda1baf1f4916de96b87d51d0
Natalie <natalie.borter@psy.unibe.ch>
2014-11-28 14:37:36 +0100

    bar plot

--- bar plot.R
+++ bar plot.R
@@ -1,4 +1,5 @@
 #Anzahle Graphiken
+png(filename="figure.png",width=180,height=120,units="mm",res=900,pointsize=12)
 par(mfrow=c(1,3))
 
 #Parameter
@@ -18,14 +19,14 @@ max<-c(.85)
 
 # Achsen
 mp<-barplot(A, border=NA,axes=FALSE,axisnames=FALSE, col=c("grey70","grey20"),ylim=c(min,max))
-mtext(side=2,line=2.5,"weber fractions", cex=1.2)
-mtext(side=1,line=1,"visual      auditiv", cex=1.2)
+mtext(side=2,line=2.5,"weber fractions", cex=1)
+mtext(side=1,line=1,"visual        auditiv", cex=1)
 abline(h=0)
 
 x.text<-colMeans(mp)
 text(c("***"),x=x.text,y=0.81, cex=2)
 
-mtext(side=3,line=2,expression(paste(bold("A  "),"Subsecond range")),cex=1.2,at=0.6)
+mtext(side=3,line=2,expression(paste(bold("(A)  "),"Subsecond range")),cex=1,at=0.52)
 abline(h=0)
 axis(2,pos=0.15,at=seq(0,.85, by=.20))
 
@@ -42,13 +43,13 @@ segments(mp,A - stA, mp, A + stA, lwd=2)
 # Achsen
 mp<-barplot(B, border=NA,axes=FALSE,axisnames=FALSE, col=c("grey 70","grey 20"),ylim=c(min,max))
 abline(h=0)
-mtext(side=1,line=1,"visual      auditiv", cex=1.2)
+mtext(side=1,line=1,"visual        auditiv", cex=1)
 
 
 x.text<-colMeans(mp)
 text(c("***"),x=x.text,y=0.30, cex=2)
 
-mtext(side=3,line=2,expression(paste(bold("B  "),"Second range")),cex=1.2,at=0.2)
+mtext(side=3,line=2,expression(paste(bold("(B)  "),"Second range")),cex=1,at=0.52)
 abline(h=0)
 axis(2,pos=0.15,labels=FALSE,at=seq(0,.85, by=.20))
 
@@ -61,16 +62,16 @@ segments(mp,B - stB, mp, B + stB, lwd=2)
 # Achsen
 mp<-barplot(C, border=NA,axes=FALSE,axisnames=FALSE, col=c("grey 70","grey 20"),ylim=c(min,max))
 abline(h=0)
-mtext(side=1,line=1,"visual      auditiv", cex=1.2)
+mtext(side=1,line=1,"visual        auditiv", cex=1)
 
 x.text<-colMeans(mp)
 text(c("NS"),x=x.text,y=0.3, cex=1.5)
 
-mtext(side=3,line=2,expression(paste(bold("C  "),"Second range \nadjusted means"),cex=1.2,at=0.2))
+mtext(side=3,line=0.4,expression(paste(bold("(C)  \n"),"Second range \nadjusted means")),cex=1,at=0.52)
 abline(h=0)
 axis(2,pos=0.15,labels=FALSE,at=seq(0,.85, by=.20))
 
 
 # Variabilität
 #segments(mp,B - stB, mp, B + stB, lwd=2)
-
+dev.off()
